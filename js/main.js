66642005d3a5278d93be4fca5cf16161b788721c
Philipp Wasser <philipp@m-f-magic.de>
2023-03-30 12:35:36 +0200

    Automatic center

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
+
+
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
 var map = L.map('map').setView([50, 9], 13);
-URL="https://overpass-api.de/api/interpreter?data=[out:json];node[leisure=picnic_table](49,5,50,6);out;"
 
 
+// handle success case
+function onSuccess(position) {
+    const {
+        latitude,
+        longitude
+    } = position.coords;
+    map.setView([latitude,longitude],13)
+    OFFSET=0.2
+    URL="https://overpass-api.de/api/interpreter?data=[out:json];node[leisure=picnic_table]("+(latitude-OFFSET)+","+(longitude-OFFSET)+","+(latitude+OFFSET)+","+(longitude+OFFSET)+");out;"
+    console.log(URL)
+    
+  fetch(URL)
+  .then((response) => response.json())
+  .then((data) =>data.elements.forEach(draw) );
+
+
+}
+
+// handle error case
+function onError() {
+    alert("Error")
+   
+}
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-fetch(URL)
-  .then((response) => response.json())
-  .then((data) =>data.elements.forEach(draw) );
-
 function draw(table){
-    L.marker([table.lat, table.lon]).addTo(map)
+  L.marker([table.lat, table.lon]).addTo(map)
 }
